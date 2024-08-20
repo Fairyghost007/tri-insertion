b99f086750f4676b4388a9d012416af70f6b839f
Fairyghost007 <biranenini6762@gmail.com>
2024-08-20 11:54:36 +0000

    3️⃣ v33️⃣

--- insertion.js
+++ insertion.js
@@ -1,12 +1,12 @@
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        let temp = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > temp) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        let j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            let temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
         }
-        arr[j + 1] = temp;
     }
     return arr;
 }
